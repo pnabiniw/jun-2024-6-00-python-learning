a50469a1adb44aefbf848d2ccb1d7dc77eb04ed8
npaudyal <npaudyal321@gmail.com>
2024-06-26 19:18:12 +0545

    lambda map reduce filter

--- day13/task1.py
+++ day13/task1.py
@@ -2,14 +2,21 @@
 # Create a function which takes dictionary key (string) as argument and return the value of that key
 
 
-student = {"name": "Alex", "age": 30, "address": "KTM"}
 
-
-def get_value(key):
-    r = student.get(key, "The info is not available")
+def get_value(x):
+    print(student, "<----------")
+    r = student.get(x, "The info is not available")
     return r
 
+student = {"name": "Alex", "age": 30, "address": "KTM", "roll": 31}
 
 key = input("Enter the key you want to access ")
 result = get_value(key)
 print(result)
+
+
+
+student = {"name": "Ram", "age": 30}
+key = "name"
+roll = student.get(key, 21)
+print(roll)
