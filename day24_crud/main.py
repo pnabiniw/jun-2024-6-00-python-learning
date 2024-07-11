7ff0c2f7818e7451e1419cc4b44785b2b4c1f689
npaudyal <npaudyal321@gmail.com>
2024-07-11 19:33:29 +0545

    crud complete

--- day24_crud/main.py
+++ day24_crud/main.py
@@ -14,11 +14,14 @@ def run_crud():
         continuee = create_student()
         run_crud() if continuee else exit_from_crud()
     elif choice.lower() == "r":
-        read_student()
+        c = read_student()
+        run_crud() if c else exit_from_crud()
     elif choice.lower() == "u":
-        update_student()
+        c = update_student()
+        run_crud() if c else exit_from_crud()
     elif choice.lower() == "d":
-        delete_student()
+        c = delete_student()
+        run_crud() if c else exit_from_crud()
     else:
         exit_from_crud()
 
